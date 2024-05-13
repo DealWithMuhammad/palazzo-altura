65333f0452a1cf6504ea37610dc15c6f49651447
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-05-13 13:05:26 +0800

    feat: add hero section, add features, add 3d highlights, add testimonials, add footer etc

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [
+      "images.unsplash.com",
+      "cdn.pixabay.com",
+      "images.pexel.com",
+      "img.freepik.com",
+      "localhost",
+      "firebasestorage.googleapis.com",
+      "palazzo-altura-di-maria.de",
+    ],
+  },
+};
 
 export default nextConfig;
---
+++ src/components/ui/WhyChooseUs.tsx
@@ -0,0 +1,27 @@
+"use client";
+import React from "react";
+import { StickyScroll } from "../ui/sticky-scroll-reveal";
+import Image from "next/image";
+
+const musicAcademyContent = [
+  {
+    title: "DDIE LAGE",
+    description:
+      "Der Palazzo Altura di Maria entsteht in einer bevorzugten Wohngegend in Nördlingen, die eine harmonische Verbindung von naturnaher Umgebung und der Nähe zur Stadt bietet. Genießen Sie die Ruhe und Privatsphäre eines naturnahen Standorts und profitieren Sie gleichzeitig von der Nähe zu Einkaufsmöglichkeiten, Restaurants und anderen urbanen Annehmlichkeiten.",
+  },
+  {
+    title: "Natur und Umfeld",
+    description:
+      "Die Wohnanlage Palazzo Altura Di Maria ist von einer idyllischen und naturnahen Umgebung umgeben. Grüne Wiesen oder nahegelegene Waldgebiete bieten die Möglichkeit für Spaziergänge, Joggen oder andere Aktivitäten im Freien. Genießen Sie die frische Luft und die Ruhe der Natur direkt vor Ihrer Haustür.",
+  },
+];
+
+function WhyChooseUs() {
+  return (
+    <div>
+      <StickyScroll content={musicAcademyContent} />
+    </div>
+  );
+}
+
+export default WhyChooseUs;
