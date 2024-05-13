3e1c38b017e8a88151b101de10887197bb68fda6
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-05-13 14:15:57 +0800

    added scrollling

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,10 +1,21 @@
 "use client";
-import { useState } from "react";
+
+import { useState, useEffect } from "react";
+import { scroller } from "react-scroll";
+
 import Image from "next/image";
+
 import Link from "next/link";
-import { CiInstagram } from "react-icons/ci";
+
+import { TbMenu } from "react-icons/tb";
+
+import { CiMail, CiInstagram } from "react-icons/ci";
+
 import { RxCross1 } from "react-icons/rx";
+
 import Logo from "../../../public/logo.png"; // Update the path to your image file
+
+let isScrolled = false;
 
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -13,22 +24,20 @@ function Navbar() {
     setIsMenuOpen((prevIsMenuOpen) => !prevIsMenuOpen);
   };
 
-  // Smooth scrolling function
-  const scrollToSection = (id) => {
-    const element = document.getElementById(id);
-    if (element) {
-      element.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
-    }
+  const handleMenuLinkClick = (targetId) => {
+    toggleMenu();
+    scroller.scrollTo(targetId, {
+      duration: 800,
+      delay: 0,
+      smooth: "easeInOutQuart",
+    });
   };
 
   return (
-    <header className="sticky bg-black top-0 z-30 flex w-full py-6 items-center justify-between p-4 navigation">
-      {/* Hamburger menu */}
+    <header className="sticky bg-black top-0 z-30 flex w-full py-6 items-center justify-between p-4 navigation ">
       <div className="flex items-center justify-center px-4">
-        <div className="hamburger-container" onClick={toggleMenu}>
+        {/* Hamburger menu */}
+        <div className="hamburger-container " onClick={toggleMenu}>
           <div className={`hamburger ${isMenuOpen ? "active" : ""}`}>
             <div className="line line1"></div>
             <div className="line line2"></div>
@@ -36,6 +45,7 @@ function Navbar() {
           </div>
         </div>
       </div>
+
       {/* Mobile menu */}
       <div
         className={`fixed inset-0 bg-[#1b152d] z-40 transition-transform ease-in-out duration-300 ${
@@ -43,53 +53,41 @@ function Navbar() {
         }`}
       >
         <div className="flex flex-col items-center justify-center text-white h-screen space-y-8 text-4xl font-bold tracking-wide lg:text-6xl xl:text-7xl">
-          <Link href="/">
-            <p className="headerLink" onClick={toggleMenu}>
-              STARTSEITE
-            </p>
-          </Link>
-          <a
-            className="headerLink"
-            onClick={() => {
-              toggleMenu();
-              scrollToSection("highlights");
-            }}
-          >
-            HIGHLIGHTS
-          </a>
+          <p className="headerLink" onClick={() => handleMenuLinkClick("home")}>
+            STARTSEITE
+          </p>
           <p
             className="headerLink"
-            onClick={() => {
-              toggleMenu();
-              scrollToSection("lage");
-            }}
+            onClick={() => handleMenuLinkClick("highlight")}
+          >
+            HIGHLIGHTS
+          </p>
+          <p
+            className="headerLink"
+            onClick={() => handleMenuLinkClick("projects")}
           >
             LAGE
           </p>
           <p
             className="headerLink"
-            onClick={() => {
-              toggleMenu();
-              scrollToSection("wohnungen");
-            }}
+            onClick={() => handleMenuLinkClick("contact")}
           >
             WOHNUNGEN
           </p>
           <p
             className="headerLink"
-            onClick={() => {
-              toggleMenu();
-              scrollToSection("kontakt");
-            }}
+            onClick={() => handleMenuLinkClick("contact")}
           >
             KONTAKT
           </p>
+          {/* Add more menu items as needed */}
           <RxCross1
             className="h-10 w-10 cursor-pointer absolute text-white transition-transform ease-in-out duration-300 top-4 left-4"
             onClick={toggleMenu}
           />
         </div>
       </div>
+
       <div className="flex-1 items-center justify-center flex">
         <Link href="/">
           <div className="relative h-10 cursor-pointer opacity-100 transition">
@@ -103,9 +101,10 @@ function Navbar() {
           </div>
         </Link>
       </div>
+
       <div className="flex items-center justify-center gap-x-5 md:gap-x=8">
         <div className="text-4xl text-white icon">
-          <a href="https//www.instagram.com" target="_blank">
+          <a href="https://www.instagram.com/" target="_blank">
             <CiInstagram />
           </a>
         </div>
