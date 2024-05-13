7b3c8f4e0c18c015d3d272de47363a391860610f
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-05-13 23:27:43 +0800

    feat: add images

--- src/components/ui/sticky-scroll-reveal.tsx
+++ src/components/ui/sticky-scroll-reveal.tsx
@@ -1,59 +1,51 @@
-'use client'
-import React, { useRef } from 'react'
-import { useMotionValueEvent, useScroll } from 'framer-motion'
-import { motion } from 'framer-motion'
-import { cn } from '@/utils/cn'
+import React, { useRef } from "react";
+import { useMotionValueEvent, useScroll } from "framer-motion";
+import { motion } from "framer-motion";
+import { cn } from "@/utils/cn";
 
 export const StickyScroll = ({
   content,
-  contentClassName
+  contentClassName,
 }: {
   content: {
-    title: string
-    description: string
-    content?: React.ReactNode | any
-  }[]
-  contentClassName?: string
+    title: string;
+    description: string;
+    content?: React.ReactNode | any;
+  }[];
+  contentClassName?: string;
 }) => {
-  const [activeCard, setActiveCard] = React.useState(0)
-  const ref = useRef<any>(null)
+  const [activeCard, setActiveCard] = React.useState(0);
+  const ref = useRef<any>(null);
   const { scrollYProgress } = useScroll({
-    // uncomment line 22 and comment line 23 if you DONT want the overflow container and want to have it change on the entire page scroll
-    // target: ref
     container: ref,
-    offset: ['start start', 'end start']
-  })
-  const cardLength = content.length
+    offset: ["start start", "end start"],
+  });
+  const cardLength = content.length;
 
-  useMotionValueEvent(scrollYProgress, 'change', (latest) => {
-    const cardsBreakpoints = content.map((_, index) => index / cardLength)
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    const cardsBreakpoints = content.map((_, index) => index / cardLength);
     const closestBreakpointIndex = cardsBreakpoints.reduce(
       (acc, breakpoint, index) => {
-        const distance = Math.abs(latest - breakpoint)
+        const distance = Math.abs(latest - breakpoint);
         if (distance < Math.abs(latest - cardsBreakpoints[acc])) {
-          return index
+          return index;
         }
-        return acc
+        return acc;
       },
       0
-    )
-    setActiveCard(closestBreakpointIndex)
-  })
+    );
+    setActiveCard(closestBreakpointIndex);
+  });
 
-  const backgroundColors = [
-    'var(--slate-900)',
-    'var(--black)',
-    'var(--neutral-900)'
-  ]
-  const linearGradients = [
-    'linear-gradient(to bottom right, var(--cyan-500), var(--emerald-500))',
-    'linear-gradient(to bottom right, var(--pink-500), var(--indigo-500))',
-    'linear-gradient(to bottom right, var(--orange-500), var(--yellow-500))'
-  ]
+  const backgroundColors = ["var(--slate-900)", "#000", "var(--neutral-900)"];
+  const backgroundImages = [
+    "url(https://palazzo-altura-di-maria.de/file/i/79353514b39077112.png)",
+    "url(https://palazzo-altura-di-maria.de/file/i/5fb73428e75077112.png)",
+  ];
   return (
     <motion.div
       animate={{
-        backgroundColor: backgroundColors[activeCard % backgroundColors.length]
+        backgroundColor: backgroundColors[activeCard % backgroundColors.length],
       }}
       className="h-[30rem] overflow-y-auto flex justify-center relative space-x-10 rounded-md p-10"
       ref={ref}
@@ -64,10 +56,10 @@ export const StickyScroll = ({
             <div key={item.title + index} className="my-20">
               <motion.h2
                 initial={{
-                  opacity: 0
+                  opacity: 0,
                 }}
                 animate={{
-                  opacity: activeCard === index ? 1 : 0.3
+                  opacity: activeCard === index ? 1 : 0.3,
                 }}
                 className="text-2xl font-bold text-slate-100"
               >
@@ -75,10 +67,10 @@ export const StickyScroll = ({
               </motion.h2>
               <motion.p
                 initial={{
-                  opacity: 0
+                  opacity: 0,
                 }}
                 animate={{
-                  opacity: activeCard === index ? 1 : 0.3
+                  opacity: activeCard === index ? 1 : 0.3,
                 }}
                 className="text-kg text-slate-300 max-w-sm mt-10"
               >
@@ -91,15 +83,21 @@ export const StickyScroll = ({
       </div>
       <motion.div
         animate={{
-          background: linearGradients[activeCard % linearGradients.length]
+          backgroundImage:
+            backgroundImages[activeCard % backgroundImages.length],
+        }}
+        style={{
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
         }}
         className={cn(
-          'hidden lg:block h-60 w-80 rounded-md bg-white sticky top-10 overflow-hidden',
+          "hidden lg:block h-60 w-80 rounded-md bg-white sticky top-10 overflow-hidden",
           contentClassName
         )}
       >
         {content[activeCard].content ?? null}
       </motion.div>
     </motion.div>
-  )
-}
+  );
+};
