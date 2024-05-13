2bdf4d7fdee8fe082c5aafde9f12106dd8e23ee4
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-05-13 13:22:46 +0800

    fixed the error

--- src/components/UpcomingWebinars.tsx
+++ src/components/UpcomingWebinars.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Link from "next/link";
 import React from "react";
 import { HoverEffect } from "./ui/card-hover-effect";
 
@@ -47,7 +46,7 @@ function UpcomingWebinars() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
         <div className="text-center">
           <h2 className="text-base text-teal-600 font-semibold tracking-wide uppercase">
-            FAQ's
+            FAQs
           </h2>
           <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">
             Häufig gestellte Fragen
