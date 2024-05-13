12ca57cca90e00d77e5ad404006a155e270a7cd3
DealWithMuhammad <muhammad.ahmad90@icloud.com>
2024-05-13 16:42:45 +0800

    feat: add vortex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -104,7 +104,7 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       {/* <h1 className="text-3xl text-center">Loki Music Academy</h1> */}
       <HeroParallax products={products} />
-      <Instructors />
+      {/* <Instructors /> */}
       <VortexDemo />
       <FeaturedCourses />
       <WhyChooseUs />
